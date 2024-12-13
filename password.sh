2da899117d98bfc74569b595ef6e6ce4b069e04f
veeraprakash <veeraprakash.bg@gmail.com>
2024-12-13 07:09:47 +0530

    New_scripts

---
+++ password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter your username::"
+read USERNAME
+echo "Enter your password:"
+read -s PASSWORD
+echo "Username entered is $USERNAME"
+echo "Password entered is $PASSWORD"
---
+++ promting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#PERSON2=Veera
+PERSON2=$2
+PERSON1=$1
+
+
+echo "$PERSON1: What is your name ?"
+echo "${PERSON2}: My name is ${PERSON2}"
+echo "${PERSON2}: What is your name ?"
+echo "$PERSON1: My name is ${PERSON1}"
