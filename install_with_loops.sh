c96c3f4312c56d319fd6550b2bec0fd323963901
vrapracash <veeraprakash.bg@gmail.com>
2024-12-16 10:50:11 +0530

    script-changes

--- install_with_loops.sh
+++ install_with_loops.sh
@@ -15,21 +15,21 @@ N="\e[0m"
 
 
 CHECK_ROOT(){
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R Use ROOT previlages to installations $N" &>>LOG_FILE
-    exit 1
-fi
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Use ROOT previlages to installations $N" &>>LOG_FILE
+        exit 1
+    fi
 }
 
 VALIDATE(){
-if [ $1 -ne 0]
-then 
-    echo -e "$2 is $R FAILED $N" &>>LOG_FILE
-    exit 1
-else
-    echo -e "$2 is $G SUCCESS... $N" &>>LOG_FILE
-fi
+    if [ $1 -ne 0]
+    then 
+        echo -e "$2 is $R FAILED $N" &>>LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G SUCCESS... $N" &>>LOG_FILE
+    fi
 }
 
 CHECK_ROOT
