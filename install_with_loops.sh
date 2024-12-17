27de6e0f88464c26c83c2063ea369b2950f8235d
vrapracash <veeraprakash.bg@gmail.com>
2024-12-17 08:49:57 +0530

    logfolder-update

--- install_with_loops.sh
+++ install_with_loops.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/log/shell_scripting_vra"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 
 mkdir -p $LOGS_FOLDER
 
